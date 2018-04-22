4d675236c421fdd331cc945b2954a921ca7e2c55
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2018-04-22 14:54:23 +0700

    Add tribonachi matrix function implementation. Change input arg type to int.

--- tribonachi.go
+++ tribonachi.go
@@ -8,29 +8,27 @@ import (
 // Simple calculate tribonachi number with specified index (nIdex)
 // using Dynamic Programming.
 //
-// nIndex *big.Int - natural integer contains tribonachi number position.
+// nIndex int - natural integer contains tribonachi number position.
 //
 // Function returns *big.Int contains tribonachi number with specified position.
 //
 // Function use three variables to keep track of previous three numbers.
-// Time complexity of this function is linear.
-func Simple(nIndex *big.Int) *big.Int {
+// Time complexity of this function is O(n).
+func Simple(nIndex int) *big.Int {
 
 	nMinus3, nMinus2, nMinus1 := big.NewInt(0), big.NewInt(0), big.NewInt(1)
 
 	firstThreeValues := [3]*big.Int{nMinus3, nMinus2, nMinus1}
 
 	for i := 0; i < len(firstThreeValues); i++ {
-		if nIndex.Cmp(big.NewInt(int64(i+1))) == 0 {
+		if nIndex == i+1 {
 			return firstThreeValues[i]
 		}
 	}
 
 	nValue := calcNValue(nMinus1, nMinus2, nMinus3)
 
-	bigOne := big.NewInt(1)
-
-	for i := big.NewInt(4); i.Cmp(nIndex) < 0; i.Add(i, bigOne) {
+	for i := 4; i < nIndex; i++ {
 
 		nMinus3, nMinus2, nMinus1 = nMinus2, nMinus1, nValue
 
@@ -47,4 +45,82 @@ func calcNValue(nMinus1, nMinus2, nMinus3 *big.Int) *big.Int {
 	nValue.Add(nValue, nMinus3)
 
 	return nValue
+}
+
+// Matrix calculate tribonachi number with specified index (nIdex)
+// using matrix exponentiation.
+//
+// nIndex int - natural integer contains tribonachi number position.
+//
+// Function returns *big.Int contains tribonachi number with specified position.
+//
+// Function use three variables to keep track of previous three numbers.
+// Time complexity of this function is O(log n).
+func Matrix(nIndex int) *big.Int {
+
+	if nIndex == 1 || nIndex == 2 {
+		return big.NewInt(0)
+	}
+
+	matrixE := [3][3]*big.Int{
+		{big.NewInt(1), big.NewInt(1), big.NewInt(1)},
+		{big.NewInt(1), big.NewInt(0), big.NewInt(0)},
+		{big.NewInt(0), big.NewInt(1), big.NewInt(0)},
+	}
+
+	matrixE = power(matrixE, nIndex-3)
+
+	// T[0][0] contains the tribonacci number
+	// so return it
+	return matrixE[0][0]
+}
+
+func multiply(matrixA, matrixB [3][3]*big.Int) [3][3]*big.Int {
+
+	result := [3][3]*big.Int{
+		{big.NewInt(0), big.NewInt(0), big.NewInt(0)},
+		{big.NewInt(0), big.NewInt(0), big.NewInt(0)},
+		{big.NewInt(0), big.NewInt(0), big.NewInt(0)},
+	}
+
+	var n = 3
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			for k := 0; k < n; k++ {
+
+				mulResult := new(big.Int).Mul(matrixA[i][k], matrixB[k][j])
+
+				result[i][j].Add(result[i][j], mulResult)
+			}
+		}
+	}
+
+	return result
+}
+
+// Recursive function to raise the matrix
+// matrixA to the power n
+func power(matrixA [3][3]*big.Int, n int) [3][3]*big.Int {
+
+	if n == 0 || n == 1 {
+		return matrixA
+	}
+
+	matrixA = power(matrixA, n/2)
+
+	matrixA = multiply(matrixA, matrixA)
+
+	if n%2 != 0 {
+
+		M := [3][3]*big.Int{
+			{big.NewInt(1), big.NewInt(1), big.NewInt(1)},
+			{big.NewInt(1), big.NewInt(0), big.NewInt(0)},
+			{big.NewInt(0), big.NewInt(1), big.NewInt(0)},
+		}
+
+		matrixA = multiply(matrixA, M)
+	}
+
+	return matrixA
 }
